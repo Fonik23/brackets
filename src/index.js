a59524fe598cd85ab10aed2200ece20ee37b0af3
konstanin f <kf3329730@gmail.com>
2024-10-22 17:49:19 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const config = Object.fromEntries(bracketsConfig.map(([key , value]) => [value, key]));
+  const openBrackets = Object.values(config);
+  let stack = [];
+  console.log(config)
+  for (let i = 0; i < str.length; i += 1) {
+    const current = str[i];
+    const top = stack[stack.length - 1];
+    if (openBrackets.includes(current)) {
+      if (top && current === config[current] && current === top) {
+        stack.pop();
+      } else {
+        stack.push(current);
+      }
+    } else {
+      
+      if (config[current] === top) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  
+  return stack.length === 0;
 }
